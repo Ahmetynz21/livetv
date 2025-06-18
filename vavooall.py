343214caff089bf71126490d6e5011c578307aaa
reelman543 <reelman543@gmail.com>
2025-06-18 04:16:59 +0300

    Update vavooall.py

--- vavooall.py
+++ vavooall.py
@@ -2,7 +2,7 @@ import requests
 import re
 
 URL = "https://vavoo.to/channels"
-PROXY_BASE = "https://falco26-flc26prx.hf.space/proxy?
+PROXY_BASE = "https://latte-proxy2.hf.space/proxy/m3u?
 url=https://vavoo.to/play/{}/index.m3u8"
 LOGO_URL = "https://raw.githubusercontent.com/reelman543/ES26-edt/refs/heads/main/vavoo.png"
 OUTPUT_FILE = "vavooall.m3u"
